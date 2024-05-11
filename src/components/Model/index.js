7746197e215947a42c97393b33bbf67f192ff34c
mazharzaka <mazharmohamed566@gmail.com>
2024-05-12 01:57:02 +0300

    finsh design model componet

--- src/components/Model/index.js
+++ src/components/Model/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import {BsFillPaletteFill, BsPalette} from "react-icons/bs";
+import {IoMdSync} from "react-icons/io";
+import {MdOutlineChangeHistory} from "react-icons/md";
 import {RiSignpostFill} from "react-icons/ri";
 
 function Model() {
@@ -13,9 +16,8 @@ function Model() {
           City
           <span className="text-[8px] text-end text-[#03a9f4]">.CSS</span>
         </div>
-        <div className="p-2 cursor-pointer rounded-xl hover:pr-3 hover:bg-[#0573A5] transition-all hover:pl-3  bg-[#0696D8] text-white mr-2">
-          {" "}
-          show font
+        <div className="p-3 cursor-point  rounded-xl hover:pr-3.5  hover:bg-[#0573A5] transition-all hover:pl-3.5  bg-[#0696D8] text-white mr-2">
+          <MdOutlineChangeHistory />
         </div>
       </div>
       <div className="w-full flex mt-2 justify-center items-center">
@@ -55,12 +57,54 @@ function Model() {
               Dark Color Test
             </div>
           </div>
+          <div className="w-full flex mt-2 items-center  justify-center">
+            <BsFillPaletteFill className="text-3xl ml-3 text-[#0573A5] " />
+            <BsPalette className="text-3xl ml-3 text-[#0573A5] " />
+            <BsFillPaletteFill className="text-xl ml-3 text-[#0573A5] " />
+            <BsPalette className="text-xl ml-3 text-[#0573A5] " />
+            <BsFillPaletteFill className="text-sm ml-3 text-[#0573A5] " />
+            <BsPalette className="text-sm ml-3 text-[#0573A5] " />
+          </div>
+          <div className="w-full flex mt-2 items-center justify-center">
+            <BsFillPaletteFill className="text-3xl ml-3 text-[#0696D8] " />
+            <BsPalette className="text-3xl ml-3 text-[#0696D8] " />
+            <BsFillPaletteFill className="text-xl ml-3 text-[#0696D8] " />
+            <BsPalette className="text-xl ml-3 text-[#0696D8] " />
+            <BsFillPaletteFill className="text-sm ml-3 text-[#0696D8] " />
+            <BsPalette className="text-sm ml-3 text-[#0696D8] " />
+          </div>
+          <div className="w-full flex mt-2 items-center justify-center">
+            <div className="w-10 cursor-pointer flex hover:rotate-90 items-center text-2xl justify-center h-8 cursor-point  rounded-xl  hover:bg-[#0573A5] transition-all   bg-[#0696D8] text-white ">
+              <IoMdSync />
+            </div>
+          </div>
         </div>
         <div className="w-56 ml-4 mt-1 mb-1 rounded h-36 bg-black-200">
           <div className="flex justify-center items-center">
             <RiSignpostFill className="text-2xl text-[#03a9f4] mr-1" />
             <div className="text-base font-extrabold  text-[#47C4FC]">
               Light Color Test
+            </div>
+          </div>
+          <div className="w-full flex mt-2 items-center  justify-center">
+            <BsFillPaletteFill className="text-3xl ml-3 text-[#47C4FC] " />
+            <BsPalette className="text-3xl ml-3 text-[#47C4FC] " />
+            <BsFillPaletteFill className="text-xl ml-3 text-[#47C4FC] " />
+            <BsPalette className="text-xl ml-3 text-[#47C4FC] " />
+            <BsFillPaletteFill className="text-sm ml-3 text-[#47C4FC] " />
+            <BsPalette className="text-sm ml-3 text-[#47C4FC] " />
+          </div>
+          <div className="w-full flex mt-2 items-center justify-center">
+            <BsFillPaletteFill className="text-3xl ml-3 text-[#03a9f4] " />
+            <BsPalette className="text-3xl ml-3 text-[#03a9f4] " />
+            <BsFillPaletteFill className="text-xl ml-3 text-[#03a9f4] " />
+            <BsPalette className="text-xl ml-3 text-[#03a9f4] " />
+            <BsFillPaletteFill className="text-sm ml-3 text-[#03a9f4] " />
+            <BsPalette className="text-sm ml-3 text-[#03a9f4] " />
+          </div>
+          <div className="w-full flex mt-2 items-center justify-center">
+            <div className="w-10 cursor-pointer flex hover:rotate-90 items-center text-2xl justify-center h-8 cursor-point  rounded-xl  hover:bg-[#03a9f4] transition-all   bg-[#47C4FC]  ">
+              <IoMdSync />
             </div>
           </div>
         </div>
